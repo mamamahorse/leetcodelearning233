2a69fbc5786efc8746af021186442c89dcd6b73e
mamamahorse <632129670@qq.com>
2025-12-03 18:16:02 +0800

    图论结束！

--- output/leetcode/floyd.cpp
+++ output/leetcode/floyd.cpp
@@ -35,4 +35,39 @@ int main()
     }
 }
 
- 
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n, m, p1, p2, val;
+    cin >> n >> m;
+
+    vector<vector<int>> grid(n + 1, vector<int>(n + 1, 10005));  // 因为边的最大距离是10^4
+
+    for(int i = 0; i < m; i++){
+        cin >> p1 >> p2 >> val;
+        grid[p1][p2] = val;
+        grid[p2][p1] = val; // 注意这里是双向图
+
+    }
+    // 开始 floyd
+    for (int k = 1; k <= n; k++) 
+    {
+        for (int i = 1; i <= n; i++) 
+        {
+            for (int j = 1; j <= n; j++) 
+            {
+                grid[i][j] = min(grid[i][j], grid[i][k] + grid[k][j]);
+            }
+        }
+    }
+    // 输出结果
+    int z, start, end;
+    cin >> z;
+    while (z--) {
+        cin >> start >> end;
+        if (grid[start][end] == 10005) cout << -1 << endl;
+        else cout << grid[start][end] << endl;
+    }
+} 
---
+++ output/leetcode/jumpgame2.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) 
+    {
+        
+    }
+};
